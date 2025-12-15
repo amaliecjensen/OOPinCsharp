9e15acfd6967967b2cfdae15d48887d7f77ea48f
amaliecjensen <amaliecolstrup@icloud.com>
2025-12-15 09:29:21 +0100

    cleanup

---
+++ Composition/DbMigrator.cs
@@ -0,0 +1,13 @@
+class DbMigrator
+{
+	private readonly Logger _logger;
+	public DbMigrator(Logger logger)
+	{
+		_logger = logger;
+	}
+
+	public void Migrate()
+	{
+		_logger.Log("we are migrating");
+	}
+}
--- inheritance/Composition/Logger.cs
+++
@@ -1,7 +0,0 @@
-class Logger
-{
-    public void Log(string message)
-    {
-        Console.WriteLine(message);
-    }
-}

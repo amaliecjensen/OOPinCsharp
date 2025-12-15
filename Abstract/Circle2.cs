a7e7301ee85dc65d47a972877190ffe50365e7a3
amaliecjensen <amaliecolstrup@icloud.com>
2025-12-15 10:38:26 +0100

    abstract classes

---
+++ Abstract/Circle2.cs
@@ -0,0 +1,12 @@
+class Circle2 : Shape2
+{
+    public override void Copy()
+    {
+        Console.WriteLine("copying a circle");
+    }
+
+    public override void Draw()
+    {
+        Console.WriteLine("drawing a circle");
+    }
+}
---
+++ Abstract/Shape2.cs
@@ -0,0 +1,11 @@
+abstract class Shape2
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+
+    public abstract void Draw();
+    public abstract void Copy();
+
+  
+}
